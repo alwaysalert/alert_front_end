23bebb2b44fe6303f428eeceb0838818deb311e7
givensik <kjs4072@icloud.com>
2022-09-22 03:33:02 +0900

    테스트중

--- src/route/Mypage.js
+++ src/route/Mypage.js
@@ -7,18 +7,17 @@ import Nav from './Nav'
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 
-
+import Button from '@mui/material/Button';
 
 function Mypage() {
   const [cookies, setCookie, removeCookie] = useCookies(['access_token']);
   console.log('cookie =',cookies.access_token);
   // 쿠키를 확인했을때 access_token이 없으면 되돌려 보내기
 
-  const [userInfo, setUserInfo] = useState({id : "givensik" ,background : 1 });
+  const [userInfo, setUserInfo] = useState({id : "givensik" ,identity :1, character : '/img/profile/profile1.png', background : 1 });
   console.log(userInfo);
   const newUserInfo = {...userInfo};
 
-  
 
   if(cookies.access_token === undefined){
     document.location = '/';
@@ -61,16 +60,31 @@ function Mypage() {
             <div className ="mypage-content-profile-content-position">
               <div className="mypage-content-profile-content"><strong>신분</strong></div>
               {/* 체크박스는 다음에 작업 이거 버튼으로 바꾸면 될듯*/}
-              <form>
-                <input type="checkbox" name ="mhstudent" />
-                <label for="mhstudent">중고등학생</label>
-                <input type="checkbox" name ="unstudent" />
-                <label for="unstudent">대학생</label>
-                <input type="checkbox" name ="graduate" />
-                <label for="graduate">졸업생</label>
-                <input type="checkbox" name ="professor" />
-                <label for="professor">교수</label>
-              </form>
+              <Button variant="text"  onClick ={(e)=>{
+                newUserInfo.identity= 1;
+                setUserInfo(newUserInfo);
+              }}>중고등학생</Button>
+              <Button variant="text" onClick ={(e)=>{
+                newUserInfo.identity = 2;
+                setUserInfo(newUserInfo);
+              }}>대학생</Button>
+              <Button variant="text" onClick ={(e)=>{
+                newUserInfo.identity = 3;
+                setUserInfo(newUserInfo);
+              }}>졸업생</Button>
+              <Button variant="text" onClick ={(e)=>{
+                newUserInfo.identity = 4;
+                setUserInfo(newUserInfo);
+              }}>교수님</Button>
+              <Button variant="text" onClick ={(e)=>{
+                newUserInfo.identity = 5;
+                setUserInfo(newUserInfo);
+              }}>현직 종사자</Button>
+              <Button variant="text" onClick ={(e)=>{
+                newUserInfo.identity = 6;
+                setUserInfo(newUserInfo);
+              }}>기타</Button>
+
             </div>
             <div className='mypage-content-profile-content-position'>
               <div className="mypage-content-profile-content"><strong>소속</strong></div>
@@ -80,33 +94,145 @@ function Mypage() {
               </form>
             </div>
             <div className ="mypage-content-profile-content-position">
-              <div className="mypage-content-profile-content"><strong>프로필</strong>
+              <div className="mypage-content-profile-content"> 
+                <div className='test'>
+                  <div className ='test3'>
+                    <img 
+                      alt='test2'
+                      className = 'test2'
+                      src={userInfo.character}
+                    />
+                  </div>
+                  
+                </div>
+                <strong>프로필</strong>
+                
               </div>
               <div className ="mypage-content-character-text">프로필 사진으로 사용할 캐릭터를 설정하세요.
                 <div className ='mypage-profile-img'>
-                  <img alt = 'character1' className ='mypage-profile-img-in'src='/img/profile/profile1.png'></img>
-                  <img alt = 'character2' className ='mypage-profile-img-in' src='/img/profile/profile2.png'></img>
-                  <img alt = 'character3' className ='mypage-profile-img-in' src='/img/profile/profile3.png'></img>
-                  <img alt = 'character4' className ='mypage-profile-img-in' src='/img/profile/profile4.png'></img>
-                  <img alt = 'character5' className ='mypage-profile-img-in' src='/img/profile/profile5.png'></img>
-                  <img alt = 'character6' className ='mypage-profile-img-in' src='/img/profile/profile6.png'></img>
-                  <img alt = 'character7' className ='mypage-profile-img-in' src='/img/profile/profile7.png'></img>
+                  <img 
+                  alt = 'character1' 
+                  className ='mypage-profile-img-in' 
+                  src='/img/profile/profile1.png' 
+                  onClick ={(e)=>{
+                  newUserInfo.character = '/img/profile/profile1.png';
+                  setUserInfo(newUserInfo);
+                }} >
+                    
+                  </img>
+                  <img 
+                  alt = 'character2' 
+                  className ='mypage-profile-img-in' 
+                  src='/img/profile/profile2.png' 
+                  onClick ={(e)=>{
+                  newUserInfo.character = '/img/profile/profile2.png' ;
+                  setUserInfo(newUserInfo);
+                }} >
+                  </img>
+                  <img 
+                  alt = 'character3' 
+                  className ='mypage-profile-img-in' 
+                  src='/img/profile/profile3.png' 
+                  onClick ={(e)=>{
+                  newUserInfo.character = '/img/profile/profile3.png';
+                  setUserInfo(newUserInfo);
+                }} >
+                  </img>
+                  <img 
+                  alt = 'character4' 
+                  className ='mypage-profile-img-in' 
+                  src='/img/profile/profile4.png' 
+                  onClick ={(e)=>{
+                  newUserInfo.character = '/img/profile/profile4.png';
+                  setUserInfo(newUserInfo);
+                }} >
+                  </img>
+                  <img 
+                  alt = 'character5' 
+                  className ='mypage-profile-img-in' 
+                  src='/img/profile/profile5.png' 
+                  onClick ={(e)=>{
+                  newUserInfo.character = '/img/profile/profile5.png' ;
+                  setUserInfo(newUserInfo);
+                }} >
+                  </img>
+                  <img 
+                  alt = 'character6' 
+                  className ='mypage-profile-img-in' 
+                  src='/img/profile/profile6.png' 
+                  onClick ={(e)=>{
+                  newUserInfo.character = '/img/profile/profile6.png' ;
+                  setUserInfo(newUserInfo);
+                }} >
+                  </img>
+                  <img 
+                  alt = 'character7' 
+                  className ='mypage-profile-img-in' 
+                  src='/img/profile/profile7.png' 
+                  onClick ={(e)=>{
+                  newUserInfo.character = '/img/profile/profile7.png';
+                  setUserInfo(newUserInfo);
+                }} >
+                  </img>
+                  
                 </div>
                 <div className ='mypage-profile-color'>
                   <div className ="mypage-content-color-text2">
                     프로필 사진의 배경색을 설정하세요
                   </div>
                   <div>
-                    <button className = 'mypage-color-button mypage-color-button1'> </button>
-                    <button className = 'mypage-color-button mypage-color-button2'> </button>
-                    <button className = 'mypage-color-button mypage-color-button3'> </button>
-                    <button className = 'mypage-color-button mypage-color-button4'> </button>
-                    <button className = 'mypage-color-button mypage-color-button5'> </button>
-                    <button className = 'mypage-color-button mypage-color-button6'> </button>
-                    <button className = 'mypage-color-button mypage-color-button7'> </button>
+                    <button 
+                      className = 'mypage-color-button mypage-color-button1'
+                      onClick ={(e)=>{
+                        newUserInfo.background = 1;
+                        setUserInfo(newUserInfo);
+                      }}
+                      > 
+                      </button>
+                    <button 
+                      className = 'mypage-color-button mypage-color-button2'
+                      onClick ={(e)=>{
+                       newUserInfo.background = 2;
+                       setUserInfo(newUserInfo);
+                      }}> </button>
+                    <button 
+                      className = 'mypage-color-button mypage-color-button3'
+                      onClick ={(e)=>{
+                        newUserInfo.background = 3;
+                        setUserInfo(newUserInfo);
+                      }}
+                      > </button>
+                    <button 
+                      className = 'mypage-color-button mypage-color-button4'
+                      onClick ={(e)=>{
+                        newUserInfo.background = 4;
+                        setUserInfo(newUserInfo);
+                      }}> </button>
+                    <button 
+                      className = 'mypage-color-button mypage-color-button5'
+                      onClick ={(e)=>{
+                        newUserInfo.background = 5;
+                        setUserInfo(newUserInfo);
+                      }}
+                      > </button>
+                    <button 
+                      className = 'mypage-color-button mypage-color-button6'
+                      onClick ={(e)=>{
+                        newUserInfo.background = 6;
+                        setUserInfo(newUserInfo);
+                      }}
+                      > </button>
+                    <button 
+                      className = 'mypage-color-button mypage-color-button7'
+                      onClick ={(e)=>{
+                          newUserInfo.background = 7;
+                          setUserInfo(newUserInfo);
+                        }}> 
+                      </button>
                   </div>
 
                 </div>
+                
               </div>
             
             </div>
