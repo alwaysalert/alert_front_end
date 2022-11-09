c1280b06d19e4c60979abd51d5f264d705dbb71e
givensik <kjs4072@icloud.com>
2022-11-09 21:58:15 +0900

    핵린이 게시판 기능 조금

--- src/route/HackChildContent.js
+++ src/route/HackChildContent.js
@@ -19,69 +19,82 @@ function HackChildContent(props) {
     const [articleArray, setArticle] = useState(null);
 
     useEffect(() => {
+      //Q&A
+      if(props.num === 0){
+        //axios -> Q&A
+      }else{
         axios.get(`${baseURL}/hackchildren/?format=json`).then((res) => {
-            setArticle(res.data);
-            console.log(res.data);
-            
-            }).catch((err) => {
-              console.log("Error check", err);
-            });
+          setArticle(res.data);
+          console.log(res.data);
+          
+          }).catch((err) => {
+            console.log("Error check", err);
+          });
+      }
+        
     },[props.num]);
 
-
-  return (
-    <div className='hackchild-content-background'>
-        {articleArray && articleArray.map(article =>{
-          if(article.tag === 0){
-            return <Qna article = {article}/>
-          }else if(article.tag === props.num){
-            function formatDate(date) {
-              return (date.getMonth() + 1).toString().padStart(2, '0') + '/' + 
-                date.getDate().toString().padStart(2, '0')  + ' ' +
-                date.getHours().toString().padStart(2, '0') + ':' + 
-                date.getMinutes().toString().padStart(2, '0')
-            }
-            var time = new Date(article.created_time);
-            console.log("article=",article)
-            
-            return (<div  key={article.id} name={article.id} id="hackchild-arts-grid" >
-                    <Link to={'/HackChild/'+article.id} style={{width:'930px',height:'130px', backgroundColor:'red'}}>
-                    <div>
-                    <div className= "myactivity-arts-profile-circle" style ={{background : util.hexcolor(article.author_info.profile_color_id)}} ><img alt = 'freeartprofile'className="myactivity-arts-profile" src={util.image_route(article.author_info.profile_picture_id)}/></div>
-                    <h4 className="myactivity-arts-title"><strong>{article.title}</strong></h4>
-                    <p className = 'myactivity-arts-text'><strong>{article.body.length > 100 ? article.body.substr(0,100) + '...' : article.body}</strong></p>
-                    <span className="freeart-arts-whenwho">{formatDate(time)}&nbsp;&nbsp;|&nbsp;&nbsp;{article.author_info.nickname}</span>
-                    <span className="count-container">
-            <div style={{display:'inline-block',width:'20px',height:'20px',marginRight:'6px'}}>
+  
+  if(props.num === 0){
+    return (
+      <div className='hackchild-content-background'>
+        <Qna></Qna>
+      </div>
+    )
+  }else{
+    return (
+      <div className='hackchild-content-background'>
+          {articleArray && articleArray.map(article =>{
+            if(article.tag === props.num){
+              function formatDate(date) {
+                return (date.getMonth() + 1).toString().padStart(2, '0') + '/' + 
+                  date.getDate().toString().padStart(2, '0')  + ' ' +
+                  date.getHours().toString().padStart(2, '0') + ':' + 
+                  date.getMinutes().toString().padStart(2, '0')
+              }
+              var time = new Date(article.created_time);
+              console.log("article=",article)
               
-              <ThumbUpAltIcon sx={{ color: '#b9b9b9',width:'23px',height:'23px',marginTop:'3' }}/>
-            </div>
-            <span style={{display:'inline-block',width:'20px',fontSize:'11px',verticalAlign:'top',marginTop:'10.5px',fontFamily:'apple-font-EB',color:'#b9b9b9'}}>
-              {article.like_users.length}
-            </span>
-            <div style={{display:'inline-block',width:'20px',height:'20px',marginRight:'6px'}}>
-              <ChatIcon sx={{ color: '#b9b9b9',width:'23px',height:'23px',marginTop:'3px' }}/>
-            </div>
-            <span style={{display:'inline-block',width:'20px',fontSize:'11px',verticalAlign:'top',marginTop:'10.5px',marginRight:'-5px',fontFamily:'apple-font-EB',color:'#b9b9b9'}}>
+              return (<div  key={article.id} name={article.id} id="hackchild-arts-grid" >
+                      <Link to={'/HackChild/'+article.id} style={{width:'930px',height:'130px', backgroundColor:'red'}}>
+                      <div>
+                      <div className= "myactivity-arts-profile-circle" style ={{background : util.hexcolor(article.author_info.profile_color_id)}} ><img alt = 'freeartprofile'className="myactivity-arts-profile" src={util.image_route(article.author_info.profile_picture_id)}/></div>
+                      <h4 className="myactivity-arts-title"><strong>{article.title}</strong></h4>
+                      <p className = 'myactivity-arts-text'><strong>{article.body.length > 100 ? article.body.substr(0,100) + '...' : article.body}</strong></p>
+                      <span className="freeart-arts-whenwho">{formatDate(time)}&nbsp;&nbsp;|&nbsp;&nbsp;{article.author_info.nickname}</span>
+                      <span className="count-container">
+              <div style={{display:'inline-block',width:'20px',height:'20px',marginRight:'6px'}}>
+                
+                <ThumbUpAltIcon sx={{ color: '#b9b9b9',width:'23px',height:'23px',marginTop:'3' }}/>
+              </div>
+              <span style={{display:'inline-block',width:'20px',fontSize:'11px',verticalAlign:'top',marginTop:'10.5px',fontFamily:'apple-font-EB',color:'#b9b9b9'}}>
+                {article.like_users.length}
+              </span>
+              <div style={{display:'inline-block',width:'20px',height:'20px',marginRight:'6px'}}>
+                <ChatIcon sx={{ color: '#b9b9b9',width:'23px',height:'23px',marginTop:'3px' }}/>
+              </div>
+              <span style={{display:'inline-block',width:'20px',fontSize:'11px',verticalAlign:'top',marginTop:'10.5px',marginRight:'-5px',fontFamily:'apple-font-EB',color:'#b9b9b9'}}>
+              
+              {article.comment_count}
+              </span>
+              <div style={{display:'inline-block',width:'20px',height:'20px',marginRight:'6px'}}>
+                <StarIcon sx={{ color: '#b9b9b9',width:'23px',height:'23px',marginTop:'3px' }}/>
+              </div>
+              <span style={{display:'inline-block',width:'3px',fontSize:'11px',verticalAlign:'top',marginTop:'10.5px',fontFamily:'apple-font-EB',color:'#b9b9b9'}}>
+              {article.bookmark ? article.bookmark.length : "0"}
+              </span>
+                      </span>                  
+                      </div>
+                      
+                      </Link>
+                      </div>)
+            }
             
-            {article.comment_count}
-            </span>
-            <div style={{display:'inline-block',width:'20px',height:'20px',marginRight:'6px'}}>
-              <StarIcon sx={{ color: '#b9b9b9',width:'23px',height:'23px',marginTop:'3px' }}/>
-            </div>
-            <span style={{display:'inline-block',width:'3px',fontSize:'11px',verticalAlign:'top',marginTop:'10.5px',fontFamily:'apple-font-EB',color:'#b9b9b9'}}>
-            {article.bookmark ? article.bookmark.length : "0"}
-            </span>
-                    </span>                  
-                    </div>
-                    
-                    </Link>
-                    </div>)
-          }
-          
-})}
-    </div>
-  )
+  })}
+      </div>
+    )
+  }
+  
 }
 
 export default HackChildContent
--- src/route/Qna.js
+++ src/route/Qna.js
@@ -1,34 +1,172 @@
-import React from 'react'
+import { height } from '@mui/system';
+import React, { useState, useRef, useEffect } from 'react'
 import '../css/hackchild.css'
 
-function Qna() {
 
+function QnaBox(props){
+
+
+
+    return (
+        <div className={props.class} style ={{height:'350px', top:'100px'}} >
+            <div className ='hackchild-qna-box-head'>
+                <span className ='hackchild-qna-box-head-text'>지금 뜨는 질문</span>
+                <div className='circle1'> </div>
+                <div className='circle2'> </div>
+                <div className='circle3'> </div>
+            </div>
+            <div className ='hackchild-qna-box-title' style ={{fontSize:'20px', top:'80px'}} >
+                <strong>정보보호학과 공부순서 어떻게 해야할까요?</strong><span>(tag)</span>
+                <div className ='hackchild-qna-box-content' style ={{fontSize:'16px', top:'80px'}}>
+                {props.content}
+                </div>
+            </div>
+            <div className='hackchild-qna-box-answer1' style ={{top:'220px'}}>
+                (프사) (댓글 내용)
+            </div>
+            <div className='hackchild-qna-box-answer2' style ={{top:'270px'}}>
+                <textarea className='hackchild-qna-box-answer-input' rows='1' cols='40' placehoder ='(닉네임)님, 해킹이 처음인 핵린이를 위해 답변을 달아주세요.' ></textarea>
+            </div>
+            <div className='hackchild-qna-box-answer-button' style ={{top:'270px'}}>답변 달기</div>
+            
+        </div>
+    )
+}
+function CenterQnaBox(props){
+    return (
+        <div ref = {props.Ref}className={props.class} style ={props.style}>
+            <div className ='hackchild-qna-box-head'>
+                <span className ='hackchild-qna-box-head-text'>지금 뜨는 질문</span>
+                <div className='circle1'> </div>
+                <div className='circle2'> </div>
+                <div className='circle3'> </div>
+            </div>
+            <div className ='hackchild-qna-box-title'>
+                <strong>정보보호학과 공부순서 어떻게 해야할까요?</strong><span>(tag)</span>
+                <div className ='hackchild-qna-box-content'>
+                {props.content}
+                </div>
+            </div>
+            <div className='hackchild-qna-box-answer1'>
+                (프사) (댓글 내용)
+            </div>
+            <div className='hackchild-qna-box-answer2'>
+                <textarea className='hackchild-qna-box-answer-input' rows='1' cols='40' placehoder ='(닉네임)님, 해킹이 처음인 핵린이를 위해 답변을 달아주세요.' ></textarea>
+            </div>
+            <div className='hackchild-qna-box-answer-button'>답변 달기</div>
+            
+        </div>
+    )
+}
+
+const boxnum = (num) => {
+    if(num<0){
+        return num+5;
+    }else if(num>4){
+        return parseInt(num%5);
+    }
+    return num;
+}
+const arraynum = (num) => {
+    if(num<0){
+        return num+10;
+    }else if(num>9){
+        return parseInt(num%10);
+    }
+    return num;
+}
+
+
+function Qna() {
+    // https://ye-yo.github.io/react/2022/01/21/infinite-carousel.html 시발 이거보고 하자
+
+    // q&a 받아와서 array에 저장
+    const QNA = [0,1,2,3,4,5,6,7,8,9];//일단 이게 article이라고 생각
+    const [whatnum, setWhatNum] = useState(2);//가운데 박스 번호, 0, 1, 2, 3, 4
+    // const style = [{display :'none',left:'-1000px',height:'350px', top:'100px'},{left:'-500px',height:'350px', top:'100px'},{left:'144px'},{left:'844px',height:'350px', top:'100px'},{display :'none',left :'900px',height:'350px', top:'100px'}];
+    const contain = useRef();
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [transit, setTransition] =useState('0.5s ease-in-out')
 
     
+    
+    const element = useRef([])
+
+     const changeSlide = (num) =>{
+        setTransition('');
+        setTimeout(() => {
+            if(num === 4){
+                setCurrentIndex(4);
+            }else if(num === 0){
+                setCurrentIndex(0);
+            }
+        }, 49)
+    }
+    const lonclick = () =>{
+        
+        setTransition('0.5s ease-in-out');
+        setCurrentIndex(currentIndex-1);
+    }
+    const ronclick = () =>{
+        setTransition('0.5s ease-in-out');
+        setCurrentIndex(currentIndex+1);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            if(currentIndex-1 === -2){//-1일때
+                changeSlide(4);
+            }else if(currentIndex+1 === 6){//5일때
+                changeSlide(0);
+            }
+        }, 450)
+        
+    },[currentIndex])
+
+    // qna css for 무한 슬라이드
+    const style ={transition : transit, transform :`matrix(1,0,0,1,${-705*currentIndex - 593 -703},0)`};
+
   return (
-    <div className='hackchild-qna-box'>
-        <div className ='hackchild-qna-box-head'>
-            <span className ='hackchild-qna-box-head-text'>지금 뜨는 질문</span>
-            <div className='circle1'> </div>
-            <div className='circle2'> </div>
-            <div className='circle3'> </div>
+  <div>
+    {/* <button onClick={lonclick}>왼</button>
+    <button onClick={ronclick}>오</button> */}
+    
+    {/* <div>{currentIndex}</div> */}
+    <div className ='hackchild-qna'>
+        <div className ='cloudleft' onClick={lonclick} ></div>
+        <div className ='cloudright' onClick={ronclick}></div>
+        <div ref={contain} style = {style}className='qna-container'>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[3]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[4]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[0]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[1]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[2]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[3]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[4]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[0]}></CenterQnaBox>
+            <CenterQnaBox class = 'hackchild-qna-box2'  content = {QNA[1]}></CenterQnaBox>
         </div>
-        <div className ='hackchild-qna-box-title'>
-            <strong>정보보호학과 공부순서 어떻게 해야할까요?</strong><span>(tag)</span>
-            <div className ='hackchild-qna-box-content'>
-            정보보호학과 공부순서 어떻게 해야 할까요? 제 학과가 정보보호학과인데… 솔직히 대학교만 다니고 이런공부를 안하다가    시작할려고하는데요 ㅠㅠ 그래서 시작한게 웹해킹쪽이나 네트워크 보안...
-            </div>
-        </div>
-        <div className='hackchild-qna-box-answer1'>
-            (프사) (댓글 내용)
-        </div>
-        <div className='hackchild-qna-box-answer2'>
-            <textarea className='hackchild-qna-box-answer-input' rows='1' cols='40' placehoder ='(닉네임)님, 해킹이 처음인 핵린이를 위해 답변을 달아주세요.' ></textarea>
-        </div>
-        <div className='hackchild-qna-box-answer-button'>답변 달기</div>
         
     </div>
+    <div className='filter-box'>
+        <div className='qna-filter'>
+            <img alt ='1' src="/img/boho/majorboho.png" className="qna-findBoho"/>
+            <div className='qna-filter-text'>
+                <strong>이용하려는 태그를 설정하세요.</strong>
+                <div className='qna-filter-text2'>
+                    분야별 태그를 활용해, 원하는 정보를 손쉽게 찾아가세요.
+                </div>
+            </div>
+        </div>
+    </div>
+     
+    
+
+  </div>
+    
+    
   )
+
 }
 
 export default Qna
